d7179c512711132c45cc6c21d9e8f33d63f5d2c7
Lauren Lind <laurenalind14@gmail.com>
2017-05-19 18:02:10 -0400

    beer hw

---
+++ 02/assignment_1.js
@@ -0,0 +1,16 @@
+var bottles = 100; 
+
+
+while (bottles>1) {
+  bottles--
+  if(bottles===1)
+  {
+    console.log(bottles + " bottle of beer on the wall ", bottles +" bottle of beer ", " Take one down and pass it around ", bottles-1 +" bottles of beer on the wall "); 
+}
+  else{
+  console.log(bottles + " bottles of beer on the wall ", bottles +" bottles of beer ", " Take one down and pass it around ", bottles-1 +" bottles of beer on the wall "); 
+}
+
+}
+
+ 
---
+++ 02/assignment_2.js
@@ -0,0 +1,9 @@
+var streetNumber=[100, 636, 23779, 890]
+var streetName = [' Dupont Street ',' Broadway Street ', ' Shooting Star Street ', ' Eagle Street '];
+var city = [' New York ', ' Brooklyn ', ' Dever ', ' Boulder ']; 
+var state = [' New York ', ' Colorado ', ' Arizona ', ' California ']; 
+var zip = [11221, 10018, 80401, 11206]
+var randomElement = streetNumber[0] + streetName[3] + city[2] + state[2] + zip[2];  
+{
+console.log(randomElement); 
+}
