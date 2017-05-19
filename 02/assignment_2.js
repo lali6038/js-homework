eb77c12e289102624a3eb202bc785b53b23f9e18
Lauren Lind <laurenalind14@gmail.com>
2017-05-19 18:18:17 -0400

    Pushing updated homework

--- 02/assignment_2.js
+++ 02/assignment_2.js
@@ -3,7 +3,25 @@ var streetName = [' Dupont Street ',' Broadway Street ', ' Shooting Star Street 
 var city = [' New York ', ' Brooklyn ', ' Dever ', ' Boulder ']; 
 var state = [' New York ', ' Colorado ', ' Arizona ', ' California ']; 
 var zip = [11221, 10018, 80401, 11206]
-var randomElement = streetNumber[0] + streetName[3] + city[2] + state[2] + zip[2];  
-{
-console.log(randomElement); 
-}
+
+
+var randomStreetNumber = Math.floor(Math.random()*streetNumber.length); 
+var randomElement = streetNumber[randomStreetNumber];
+
+var randomStreetName = Math.floor(Math.random()*streetName.length); 
+var randomElement1 = streetName[randomStreetName];
+
+var randomCity = Math.floor(Math.random()*city.length);
+var randomElement2 = city[randomCity];
+
+var randomState = Math.floor(Math.random()*state.length);
+var randomElement3 = state[randomState];
+
+var randomZip = Math.floor(Math.random()*zip.length);
+var randomElement4 = zip[randomZip];
+console.log(randomElement + "" +  randomElement1 + ", " + randomElement2 + ", " + randomElement3 + ", " + randomElement4); 
+
+
+
+
+
